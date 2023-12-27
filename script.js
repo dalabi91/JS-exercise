3326e5e6cdc465035d74e26da3f61d9baa832ac0
dalabi91 <152018805+dalabi91@users.noreply.github.com>
2023-12-27 02:38:16 +0000

    more js math and date practice

--- script.js
+++ script.js
@@ -3,18 +3,18 @@
 // const d = new Date();
 // alert(d);
 
-//Use the correct Date method to extract the year (four digits) out of a date object.
+// extract the year (four digits) out of a date object.
 
 // const d = new Date();
 // year = d.getFullYear();
 // alert(d);
 
-//Use the correct Date method to get the month (0-11) out of a date object.
+// get the month (0-11) out of a date object.
 
 // const d = new Date();
 // month = d.getMonth();
 
-//Use the correct Date method to set the year of a date object to 2020.
+// set the year of a date object to 2020.
 
 // const d = new Date();
 // d.setFullYear(2020);
@@ -22,16 +22,26 @@
 //js Math
     //let loop
 
-//Use the correct Math method to create a random number.
+// create a random number.
 
 let r = Math.random();
 console.log (r)// displays random number between o-1 every time the page is refreshed.
 
 //Use the correct Math method to return the largest number of 10 and 20.
 
-let x = Math.max(10, 20);
-console.log(x)
+// let x = Math.max(10, 20);
+// console.log(x)
+
 //return smallest no.
 let z = Math.min(2, 20);
 console.log(z)
 
+//round a number to the nearest integer.
+
+// let x = Math.round(5.3);
+// console.log(x)
+
+//get the square root of 9.
+
+let x = Math.sqrt(9);
+console.log(x)
